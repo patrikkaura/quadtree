058098759a38016a7f6b2f9ef61dc50b0ac8041a
Patrik Kaura <patrik.kaura@firma.seznam.cz>
2022-10-14 21:42:35 +0200

    feat: implement sentry logging

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Dashboard } from "@components/Dashboard";
+import Dashboard from "@components/Dashboard";
 import type { NextPage } from "next";
 
 const QuadtreeDashboard: NextPage = () => {
