86bcb0207723c720bc18978604943db35c81a471
Patrik Kaura <patrik.kaura@firma.seznam.cz>
2022-12-22 08:33:01 +0100

    chore: update sentry

--- web/sentry.server.config.js
+++ web/sentry.server.config.js
@@ -5,6 +5,6 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn:
     SENTRY_DSN ||
-    "https://e42ae3280d9a470db8fe18aa43da1335@o1305822.ingest.sentry.io/4503983610462208",
+    "https://8ffc507866514fb9a2c780beb9c70bb9@o1305822.ingest.sentry.io/4504371469746176",
   tracesSampleRate: 1.0,
 });
